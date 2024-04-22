15f983c1e3f307d942aa62459b05f7fdbb24a74f
yxiuhli <luctr1509@gmail.com>
2024-04-22 16:36:01 +0700

    fix datagrid

--- src/app/patients/page.jsx
+++ src/app/patients/page.jsx
@@ -1,22 +1,58 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { getPatients } from "@/lib/data";
 import { Typography } from "@mui/material";
 
-const patients = await getPatients();
-
-const columns = [
-  { field: 'name', headerName: 'Họ và tên', width: 150 },
-  { field: 'mail', headerName: 'Email', width: 150 },
-];
-
-const rows = patients.map((patient) => ({
-  id: patient.id,
-  name: patient.name,
-  mail: patient.email,
-}));
-
 const PatientsPage = async () => {
+  const [patients,setPatients] =  useState([])
+
+  useEffect(() => {
+    getPatients().then((data) => setPatients(data))
+  }, [])
+
+  const columns = [
+    {
+      field: "name",
+      headerName: "Họ và tên",
+      width: 150,
+      renderCell: (param) => {
+        return <Link href={param.value}>{param.value}</Link>;
+      },
+    },
+    { field: "dob", headerName: "Ngày sinh", width: 150 },
+    { field: "gender", headerName: "Giới tính", width: 100 },
+    { field: "start", headerName: "Ngày bắt đầu làm", width: 200 },
+    { field: "degree", headerName: "Bằng cấp", width: 450 },
+    {
+      field: "edit",
+      headerName: "Chỉnh sửa",
+      width: 120,
+      renderCell: () => {
+        return <Button>Edit</Button>;
+      },
+    },
+    {
+      field: "delete",
+      headerName: "Xóa",
+      width: 120,
+      renderCell: () => {
+        return <Button>Delete</Button>;
+      },
+    },
+  ];
+
+  const rows = patients.map((patient) => ({
+    id: patient._id,
+    name: patient.name,
+    dob: patient.dob.split("T")[0],
+    gender: patient.gender,
+    start: patient.startedWork.split("T")[0],
+    degree: patient.degree,
+    edit: "edit",
+    delete: "delete",
+  }));
+
   return (
     <div className="">
       <Typography className="mt-12 ml-14 mb-8" variant="h5">
--- src/app/staffs/doctors/page.jsx
+++ src/app/staffs/doctors/page.jsx
@@ -1,15 +1,25 @@
-"use server";
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { getDoctors } from "@/lib/data";
-import { Typography, Button } from "@mui/material";
-import { renderLink, renderButton } from "@/components/render/RenderLink";
+import { Typography, Button, Link } from "@mui/material";
 
 const DoctorsPage = async () => {
-  const doctors = await getDoctors();
+  const [doctors,setDoctors] =  useState([])
+
+  useEffect(() => {
+    getDoctors().then((data) => setDoctors(data))
+  }, [])
 
   const columns = [
-    { field: "name", headerName: "Họ và tên", width: 150, renderCell: renderLink },
+    {
+      field: "name",
+      headerName: "Họ và tên",
+      width: 150,
+      renderCell: (param) => {
+        return <Link href={param.value}>{param.value}</Link>;
+      },
+    },
     { field: "dob", headerName: "Ngày sinh", width: 150 },
     { field: "gender", headerName: "Giới tính", width: 100 },
     { field: "start", headerName: "Ngày bắt đầu làm", width: 200 },
@@ -18,13 +28,17 @@ const DoctorsPage = async () => {
       field: "edit",
       headerName: "Chỉnh sửa",
       width: 120,
-      renderCell: renderButton,
+      renderCell: () => {
+        return <Button>Edit</Button>;
+      },
     },
     {
       field: "delete",
       headerName: "Xóa",
       width: 120,
-      renderCell: renderButton,
+      renderCell: () => {
+        return <Button>Delete</Button>;
+      },
     },
   ];
 
