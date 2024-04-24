46c288ff306464198507e5765fc6ece78c87c12c
Dương Chí Hiếu <nineoxon2005@gmail.com>
2024-04-24 16:18:04 +0700

    edit single doctor page

--- src/app/staffs/doctors/[single_doctor]/page.jsx
+++ src/app/staffs/doctors/[single_doctor]/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { getDoctor } from "@/lib/data";
-import { Typography } from "@mui/material";
+import { Typography, ListItem, List, Card } from "@mui/material";
 
 
 const SingleDoctorPage = async ({params}) => {
@@ -8,34 +8,38 @@ const SingleDoctorPage = async ({params}) => {
   const doctor = await getDoctor(single_doctor);
   const avatarWidth = "400px";
   return (
-    <div className='w-full h-[calc(100vh-118px)] flex justify-center items-center relative gap-[30px]'>
-      <div className='w-[calc((100%-800px)/2+200px-15px)] h-full absolute bg-[#D6E5FA] left-0'></div>
-      <img src={"/" + doctor._id + ".jpg"} alt="" srcset="" className='object-cover rounded-full w-[400px] aspect-square z-10 ring-2 ring-[#1572A1]'/>
-      <div className='w-[400px] aspect-square flex flex-col justify-between p-5'>
-        <Typography className="font-semibold flex justify-between items-center w-full">
-           <span className='w-max text-[#0002A1]'>Tên:</span>
-           <span className='w-max'>{doctor.name}</span>
+    <div className='w-full h-[calc(100vh-118px)] grid grid-cols-[400px_1fr] grid-rows-[100%]'>
+      <div className='bg-[#5CA0D3] pt-3'>
+        <Typography className='w-max font-bold text-3xl mb-7 mx-auto'>
+          Thông tin bác sĩ
         </Typography>
-        <Typography className="font-semibold flex justify-between items-center w-full">
-          <span className='w-max text-[#0002A1]'>Ngày sinh:</span>
-          <span className='w-max'>{doctor.dob.split("T")[0].split("-").reverse().join(" - ")}</span>
-        </Typography>
-        <Typography className="font-semibold flex justify-between items-center w-full">
-          <span className='w-max text-[#0002A1]'>Giới tính:</span>
-          <span className='w-max capitalize'>{doctor.gender}</span>
-        </Typography>
-        <Typography className="font-semibold flex justify-between items-center w-full">
-          <span className='w-max text-[#0002A1]'>Ngày bắt đầu làm:</span>
-          <span className='w-max'>{doctor.startedWork.split("T")[0].split("-").reverse().join(" - ")}</span>
-        </Typography>
-        <Typography className="font-semibold flex justify-between items-center w-full">
-          <span className='w-max text-[#0002A1]'>Bằng cấp:</span>
-          <span className='w-[70%] text-right'>{doctor.degree}</span>
-        </Typography>
-        <Typography className="font-semibold flex justify-between items-center w-full">
-          <span className='w-max text-[#0002A1]'>Vị trí: </span>
-          <span className='w-max capitalize'>{doctor.job}</span>
-        </Typography>
+        <div className="flex justify-between items-center p-2 mx-7 gap-4 shadow-lg rounded-3xl bg-[#D6E5FA]">
+          <img src={"/" + doctor._id + ".jpg"} alt="" srcset="" className='object-cover rounded-full w-[25%] aspect-square'/>
+          <div className="w-full flex flex-col items-start justify-start gap-3">
+            <Typography className="w-[60%] text-xl">
+                <span className='font-bold w-max]'>Tên: </span>
+                <span className='font-light w-max'>{doctor.name}</span>
+            </Typography>
+            <Typography className="w-[60%] text-xl"> 
+                <span className='font-bold w-max]'>Vị trí:  </span>
+                <span className='w-max capitalize font-light '>{doctor.job}</span>
+            </Typography>
+          </div>
+        </div>
+        <Card className='p-4 mx-7 mt-4 gap-5 shadow-lg rounded-3xl bg-[#D6E5FA]'>
+          <List>
+            <ListItem>Ngày sinh: {doctor.dob.split("T")[0].split("-").reverse().join(" - ")}</ListItem>
+            <ListItem>Giới tính: {(doctor.gender == 'female' ? 'Nữ' : 'Nam')}</ListItem>
+            <ListItem>Ngày bắt đầu làm: {doctor.startedWork.split("T")[0].split("-").reverse().join(" - ")}</ListItem>
+            <ListItem>Bằng cấp: {doctor.degree}</ListItem>
+          </List>
+        </Card>
+      </div>
+      <div className='p-3'>
+        <Typography className='font-bold text-3xl mb-7'>Lịch làm việc</Typography>
+        <div className="w-full shadow-lg rounded-3xl bg-[#D6E5FA]">
+
+        </div>
       </div>
     </div>
   )
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bot-0 h-12 flex items-center justify-between border-t border-solid border-0 border-blue-900/10 text-gray-700 px-4">
+    <div className="bot-0 h-12 flex items-center justify-between border-t border-solid border-0 border-blue-900/10 text-gray-700 px-4 z-10">
       <div className="font-bold">Logo</div>
       <div className="text-xs">Website description © All rights reserved.</div>
     </div>
